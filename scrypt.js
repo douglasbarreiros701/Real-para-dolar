2ccf14540e9f59f9ce9ef6c650b12e03f7b6c7d1
Douglas Barreiros <97680488+douglasbarreiros701@users.noreply.github.com>
2022-04-20 18:00:52 -0300

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -5,5 +5,5 @@ function calcular() {
   let numero = inputNumero.value;
 
   let resultado = numero * 4.67;
-  alert("Você tem " + parseFloat(resultado) + " dolares");
+  alert("Você tem " + parseFloat(resultado) + " Reais");
 }
